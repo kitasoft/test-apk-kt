b8e3defba8959cad0b26acebaa10fb2166bd7d8c
kitasoft <info.kitasoft@gmail.com>
2018-04-21 20:15:17 +0900

    新規追加

---
+++ README.md
@@ -0,0 +1,3 @@
+# Overview
+
+Minimal Kotlin Project.
---
+++ app/src/main/java/com/kitasoft/testApkKt/Main.kt
@@ -0,0 +1,13 @@
+package com.kitasoft.testApkKt
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class Main : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main)
+    }
+}
